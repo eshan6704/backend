9c1eb855ba3b705b17afb1260a88e403e921f503
eshan <eshan6704@users.noreply.huggingface.co>
2025-11-28 08:18:29 +0000

    Update yf.py

--- yf.py
+++ yf.py
@@ -12,9 +12,26 @@ def qresult(symbol):
     ticker = yf.Ticker(yfsymbol)
     df = ticker.quarterly_financials
     return df
+    
 def result(symbol):
     yfsymbol = symbol + ".NS"
     ticker = yf.Ticker(yfsymbol)
     df = ticker.financials
     return df
     
+def balance(symbol):
+    yfsymbol = symbol + ".NS"
+    ticker = yf.Ticker(yfsymbol)
+    df = ticker.balance_sheet
+    return df
+    
+def cashflow(symbol):
+    yfsymbol = symbol + ".NS"
+    ticker = yf.Ticker(yfsymbol)
+    df = ticker.cashflow
+    return df
+    
+def dividend(symbol):        
+    ticker = yf.Ticker(yfsymbol)
+    df = ticker.dividends.to_frame('Dividend')
+    return df
