50c854b632ac58b32a5e7f59aee435888f82526c
eshan <eshan6704@users.noreply.huggingface.co>
2025-11-28 08:24:00 +0000

    Update yf.py

--- yf.py
+++ yf.py
@@ -3,41 +3,50 @@ import yfinance as yf
 import pandas as pd
 
 def info(symbol):
-        yf_symbol = symbol + ".NS"
-        tk = yf.Ticker(yf_symbol)
+
+        tk = yf.Ticker(symbol + ".NS")
         return tk
 
 def qresult(symbol):
-    yfsymbol = symbol + ".NS"
-    ticker = yf.Ticker(yfsymbol)
+
+    ticker = yf.Ticker(symbol + ".NS")
     df = ticker.quarterly_financials
     return df
     
 def result(symbol):
-    yfsymbol = symbol + ".NS"
-    ticker = yf.Ticker(yfsymbol)
+
+    ticker = yf.Ticker(symbol + ".NS")
     df = ticker.financials
     return df
     
 def balance(symbol):
-    yfsymbol = symbol + ".NS"
-    ticker = yf.Ticker(yfsymbol)
+
+    ticker = yf.Ticker(symbol + ".NS")
     df = ticker.balance_sheet
     return df
     
 def cashflow(symbol):
-    yfsymbol = symbol + ".NS"
-    ticker = yf.Ticker(yfsymbol)
+
+    ticker = yf.Ticker(symbol + ".NS")
     df = ticker.cashflow
     return df
     
 def dividend(symbol):        
-    ticker = yf.Ticker(yfsymbol)
+    ticker = yf.Ticker(symbol + ".NS")
     df = ticker.dividends.to_frame('Dividend')
     return df
     
 def dividend(symbol): 
-        ticker = yf.Ticker(yfsymbol)
+        ticker = yf.Ticker(symbol + ".NS")
         df = ticker.splits.to_frame('Split')
         return df
+    
+def intraday(symbol): 
 
+        df = ticker.download(symbol + ".NS",period="1d",interval="5min").round(2)
+        return df
+    
+def daily(symbol): 
+
+        df = ticker.download(symbol + ".NS",period="1y",interval="1d").round(2)
+        return df
