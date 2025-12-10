20827ae7b757bd9ecc272881e7f78e622e91fc7f
eshan <eshan6704@users.noreply.huggingface.co>
2025-12-10 14:27:36 +0000

    Update bhavcopy_html.py

--- bhavcopy_html.py
+++ bhavcopy_html.py
@@ -16,6 +16,7 @@ def build_bhavcopy_html(date_str):
     # -------------------------------------------------------
     try:
         df = nse_bhavcopy(date_str)   # <-- your custom loader
+        df.columns = df.columns.str.strip()
     except:
         return f"<h3>No Bhavcopy found for {date_str}.</h3>"
 
